962560a44eb9f64999d3e045ad9f5e0fc63216e9
Lautaro de los Heros <lautarodelosheros@gmail.com>
2022-05-08 01:09:13 -0300

    BasicDrawerViewController - Add pan gesture

--- BasicDrawerViewController/Classes/BasicDrawerViewController.swift
+++ BasicDrawerViewController/Classes/BasicDrawerViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 public class BasicDrawerViewController: UIViewController {
 
     @IBOutlet weak var drawerWidthConstraint: NSLayoutConstraint!
+    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
     
     private var presentTransition = LeftPushPresentationTransition()
     private var dismissTransition = LeftPushDismissalTransition()
     
     private let maximumWidth: Double
     private let screenProportion: Double
+    private let bounceLeeway: Double
     
-    public init(maximumWidth: Double, screenProportion: Double = 0.7) {
+    private var drawerActualWidth: CGFloat {
+        get {
+            min(view.frame.width * screenProportion, maximumWidth)
+        }
+    }
+    
+    private var xPanStart: CGFloat = 0
+    
+    public init(maximumWidth: Double, screenProportion: Double = 0.7, bounceLeeway: Double = 10) {
         self.screenProportion = screenProportion
         self.maximumWidth = maximumWidth
+        self.bounceLeeway = bounceLeeway
         
         super.init(nibName: String(describing: BasicDrawerViewController.self), bundle: Library.resourceBundle)
         
@@ -35,13 +46,51 @@ public class BasicDrawerViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    override public func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        drawerWidthConstraint.constant = min(view.frame.width * screenProportion, maximumWidth)
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        drawerWidthConstraint.constant = drawerActualWidth
     }
     
-    @IBAction func trailingViewTouched(_ sender: Any) {
-        dismiss(animated: true)
+    override public func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate { _ in
+            self.resetDrawerWidth()
+        }
+    }
+    
+    @IBAction func didPan(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        switch sender.state {
+        case .began:
+            xPanStart = point.x
+        case .changed:
+            let difference = max(xPanStart - point.x, -bounceLeeway)
+            drawerWidthConstraint.constant = drawerActualWidth - difference
+        case .ended:
+            guard sender.velocity(in: view).x > -100 else {
+                dismiss(animated: true)
+                break
+            }
+            let xDrawer = drawerActualWidth - xPanStart + point.x
+            if xDrawer < view.frame.width / 2 {
+                dismiss(animated: true)
+            } else {
+                resetDrawerWidth()
+            }
+        case .cancelled:
+            resetDrawerWidth()
+        case .possible, .failed:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    private func resetDrawerWidth() {
+        drawerWidthConstraint.constant = drawerActualWidth
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
