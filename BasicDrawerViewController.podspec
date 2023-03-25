1e07172046aaab7192e7d15e0482fecc0e6f0c9d
Lautaro <lautarodelosheros@gmail.com>
2023-03-25 00:56:01 -0300

    Develop (#10)
    * Fix bundle for SPM
    
    * Fix bundle for SPM
    
    * Fix bundle for SPM

--- BasicDrawerViewController.podspec
+++ BasicDrawerViewController.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'BasicDrawerViewController'
-  s.version          = '0.3.5'
+  s.version          = '0.3.8'
   s.summary          = 'A simple Android-like Drawer implementation for iOS.'
   s.swift_version    = '5.0'
 
--- Sources/BasicDrawerViewController/Classes/Library.swift
+++ Sources/BasicDrawerViewController/Classes/Library.swift
@@ -10,21 +10,21 @@ import Foundation
 final class Library {
     
     static let resourceBundle: Bundle = {
-        guard let resourceBundleURL = myBundle.url(
-            forResource: "BasicDrawerViewControllerBundle", withExtension: "bundle")
-            else { fatalError("BasicDrawerViewControllerBundle.bundle not found") }
-
-        guard let resourceBundle = Bundle(url: resourceBundleURL)
-            else { fatalError("Cannot access BasicDrawerViewControllerBundle.bundle") }
+        guard let resourceBundle = myBundle else {
+            fatalError("Cannot access BasicDrawerViewControllerBundle.bundle")
+        }
 
         return resourceBundle
     }()
     
-    private static let myBundle: Bundle = {
+    private static let myBundle: Bundle? = {
         #if SWIFT_PACKAGE
         return Bundle.module
         #else
-        return Bundle(for: Library.self)
+        return Bundle(url: Bundle(for: Library.self).url(
+            forResource: "BasicDrawerViewControllerBundle",
+            withExtension: "bundle"
+        )!)
         #endif
     }()
 }
