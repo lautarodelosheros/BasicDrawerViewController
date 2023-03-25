10b2d56df0a686a10d21afd2ba9a5f363ec413d6
Lautaro de los Heros <lautarodelosheros@gmail.com>
2023-03-25 00:43:17 -0300

    Fix bundle for SPM

--- Sources/BasicDrawerViewController/Classes/Library.swift
+++ Sources/BasicDrawerViewController/Classes/Library.swift
@@ -10,21 +10,25 @@ import Foundation
 final class Library {
     
     static let resourceBundle: Bundle = {
-        guard let resourceBundleURL = myBundle.url(
-            forResource: "BasicDrawerViewControllerBundle", withExtension: "bundle")
-            else { fatalError("BasicDrawerViewControllerBundle.bundle not found") }
+        guard let resourceBundleURL = bundleUrl else {
+            fatalError("BasicDrawerViewControllerBundle.bundle not found")
+        }
 
-        guard let resourceBundle = Bundle(url: resourceBundleURL)
-            else { fatalError("Cannot access BasicDrawerViewControllerBundle.bundle") }
+        guard let resourceBundle = Bundle(url: resourceBundleURL) else {
+            fatalError("Cannot access BasicDrawerViewControllerBundle.bundle")
+        }
 
         return resourceBundle
     }()
     
-    private static let myBundle: Bundle = {
+    private static let bundleUrl: URL? = {
         #if SWIFT_PACKAGE
-        return Bundle.module
+        return Bundle.module.path(forResource: "\(Library.self)", ofType: "bundle")
         #else
-        return Bundle(for: Library.self)
+        return Bundle(for: Library.self).url(
+            forResource: "BasicDrawerViewControllerBundle",
+            withExtension: "bundle"
+        )
         #endif
     }()
 }
